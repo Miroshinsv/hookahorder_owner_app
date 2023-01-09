db1ff05805d1c120876ee35f16e8e10fc9e590f4
Miroshin Sergey <baloo@wisebits.com>
2023-01-09 23:01:18 +0300

    add miss files

--- android/app/src/main/kotlin/ru/hookahorder/ownerapp/hookahorder_owner_app/MainActivity.kt
+++ android/app/src/main/kotlin/ru/hookahorder/ownerapp/hookahorder_owner_app/MainActivity.kt
@@ -2,5 +2,5 @@ package ru.hookahorder.ownerapp.hookahorder_owner_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hookahorder_owner_app/ui/configuration/configure_depencies.dart';
+import 'package:hookahorder_owner_app/ui/routes/app_routes.dart';
+import 'package:hookahorder_owner_app/ui/screens/initial_screen/initial_screen.dart';
+import 'package:hookahorder_owner_app/ui/screens/initial_screen/initial_screen_cubit.dart';
+import 'package:hookahorder_owner_app/ui/screens/login_screen/login_screen.dart';
+import 'package:hookahorder_owner_app/ui/screens/main_screen/main_screen.dart';
+import 'package:hookahorder_owner_app/ui/services/shared_preferences/shared_preferences_interface.dart';
+import 'package:hookahorder_owner_app/ui/theme/theme_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
+  ISharedPreferences sharedPreferences = GetIt.I.get();
+  await sharedPreferences.initPrefs();
+  runApp(
+    MaterialApp(
+      theme: lightTheme(),
+      routes: {
+        AppRoutes.LOGIN_SCREEN: (context) => const LoginScreen(),
+        AppRoutes.MAIN_SCREEN: (context) => const MainScreen(),
+      },
+      home: BlocProvider(
+        create: (BuildContext context) => InitialScreenCubit(),
+        child: const InitialScreen(),
+      ),
+    ),
+  );
 }
-
